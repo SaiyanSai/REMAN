98eb8ffecf3cc96c3f757cdaf5d6ea9e9bc605d5
SaiyanSai <sents@farmingdale.edu>
2022-11-10 19:04:36 -0500

    models added migrations not done yet

--- resourcemanager/models.py
+++ resourcemanager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Staff(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    staff_id = models.CharField(max_length=20, null = False)
+
+class Student(models.Model):
+    user = user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ram_id = models.CharField(max_length=20, null = False)
+    EET = 'EET'
+    CET = 'CET'
+    MET = 'MET'
+    cource_choices = [(EET, 'EET'),(CET, 'CET'),(MET, 'MET')]
+    course = models.CharField(
+        null = False,
+        max_length= 5,
+        choices= cource_choices,
+        default = EET
+    )
