15d7dd1517de7a1952c0b4fd907fdd1faea34a28
SaiyanSai <sents@farmingdale.edu>
2023-01-14 12:27:26 -0500

    heroku try 2

--- reman/settings.py
+++ reman/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "static"),
-#)
+STATICFILES_DIRS = (
+    '/static/',
+)
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
 
